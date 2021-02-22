4b50b4bb320f4c3ad8d2e930f6e26e9b47f3aec7
jeffery li <jeffery.li@grapecity.com>
2021-02-22 08:35:32 +0800

    commit codes

--- src/hello.go
+++ src/hello.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var msg string = "Hello World By Variable"
-	
 	var number int16 = 100
 	fmt.Println(msg, number)
+
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
